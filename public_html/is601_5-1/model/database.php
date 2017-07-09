114fc9327bb1d02372b537a975bc00c32860c1d7
monika pattanayak <mp657@njit.edu>
2017-07-09 17:47:21 -0400

    changed username and password

--- public_html/is601_5-1/model/database.php
+++ public_html/is601_5-1/model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=mp657';
+    $username = 'mp657';
+    $password = 'CX0X54BbM';
 
     try {
         $db = new PDO($dsn, $username, $password);
